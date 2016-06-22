6b24fa97a8e3639830f65aa097da22fae0ec39b6
password636 <lizongliang@hotmail.com>
2016-06-22 08:04:49 -0400

    function

---
+++ functions/function_parms.py
@@ -0,0 +1,13 @@
+def print_max(a, b):
+	if a > b:
+		print(a, 'is maximum')
+	elif a < b:
+		print(b, 'is maximum')
+	else:
+		print(a, 'is equal to', b)
+
+
+print_max(3,4)
+x=5
+y=7
+print_max(x,y)
