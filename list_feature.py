0053df4709542f5a37e711ad9ee69dc5b97b35e7
password636 <lizongliang@hotmail.com>
2016-11-15 22:24:58 -0500

    ds

---
+++ list_feature.py
@@ -0,0 +1,4 @@
+list1 = [1,9,8,3]
+list2 = [ n*2 for n in list1 ]	# map using for-loop inside []
+print(list1)
+print(list2)
---
+++ range.py
@@ -0,0 +1,7 @@
+mylist = range(4)	# mylist is a range object
+print(mylist[1])	# though a range object can use [] to index
+print(mylist)
+
+mylist1 = list(range(4))# mylist is a list object
+mylist1.append(7)
+print(mylist1)
